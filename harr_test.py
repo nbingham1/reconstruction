8398159289400d0601e9a16c8bcbeec7bcef6e7a
Ned Bingham <eab242@cornell.edu>
2015-09-19 03:42:19 -0400

    colorized compression

--- harr_test.py
+++ harr_test.py
@@ -2,12 +2,16 @@ from harr import *
 import Image
 import numpy as np
 
-img = to_float(load('cat-original.png'))
-coeffs = haar_2d(img)
-strong_coeffs = keep_ratio(coeffs, .0025)
-lossy = ihaar_2d(strong_coeffs)
+img = load('sky1024px.jpg')
+rcoeffs = haar_2d(to_float(img[0]))
+gcoeffs = haar_2d(to_float(img[1]))
+bcoeffs = haar_2d(to_float(img[2]))
+rstrong_coeffs = keep_ratio(rcoeffs, .0025)
+gstrong_coeffs = keep_ratio(gcoeffs, .0025)
+bstrong_coeffs = keep_ratio(bcoeffs, .0025)
+rlossy = ihaar_2d(rstrong_coeffs)
+glossy = ihaar_2d(gstrong_coeffs)
+blossy = ihaar_2d(bstrong_coeffs)
 
-save('cat-coeff.png', bipolar(coeffs))
-save('cat-coeff-5pct.png', bipolar(strong_coeffs))
-save('cat-output.png', from_float(lossy))
+save('cat-output.png', from_float(rlossy), from_float(glossy), from_float(blossy))
 
