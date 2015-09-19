d89565fa1058ee4689cabfdc28a86f7ef9275876
Ned Bingham <eab242@cornell.edu>
2015-09-18 22:08:56 -0400

    started work on deep dream

---
+++ dream.py
@@ -0,0 +1,21 @@
+# imports and basic notebook setup
+from cStringIO import StringIO
+import numpy as np
+import scipy.ndimage as nd
+import PIL.Image
+from IPython.display import clear_output, Image, display
+from google.protobuf import text_format
+
+import caffe
+
+# If your GPU supports CUDA and Caffe was built with CUDA support,
+# uncomment the following to run Caffe operations on the GPU.
+# caffe.set_mode_gpu()
+# caffe.set_device(0) # select GPU device if multiple devices exist
+
+def showarray(a, fmt='jpeg'):
+    a = np.uint8(np.clip(a, 0, 255))
+    f = StringIO()
+    PIL.Image.fromarray(a).save(f, fmt)
+    display(Image(data=f.getvalue()))
+
